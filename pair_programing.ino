23c238b94e15d2ccaacb18b30011e7c3531a1b75
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-10 15:10:18 +0900

    Blink & no_Blink

--- pair_programing.ino
+++ pair_programing.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+const int LED = 6;
+const int BUTTON = 2;
+int buttonState = 0;
+int lastButtonState = 0;
+int buttonPushCounter = 0;
 
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //blink();
+  button();
+  Serial.println(buttonPushCounter);
+  if ((buttonPushCounter % 2) == 1)
+    blink();
+  if ((buttonPushCounter % 2) == 0)
+    no_blink();
+}
 
+void blink() {
+  digitalWrite(LED, HIGH);
+}
+
+void no_blink() {
+  digitalWrite(LED, LOW);
+}
+
+void button() {
+  buttonState = digitalRead(BUTTON);
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      buttonPushCounter++;
+    }
+  }
+
+  lastButtonState = buttonState;
 }
